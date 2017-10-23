6760c01bc63e9c69f657eef1fc24457340dd3a71
SheepMie <347254621@qq.com>
2017-10-23 17:04:18 +0800

    creat api-dairy

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/dairy');
+var db = mongoose.connect('mongodb://localhost:27017/SheepDairy');  //链接数据库
+db.connection.on("error", function(error) {
+    console.log("数据库连接失败：" + error);
+});
+db.connection.on("open", function() {
+    console.log("------数据库连接成功！------");    //数据模板创建用Schema模块去创建 
+});
+
+var modelsPath = path.join(__dirname, 'models');
+fs.readdirSync(modelsPath).forEach(function (file) {
+    if (/(.*)\.(js$|coffee$)/.test(file)) {
+        require(modelsPath + '/' + file);
+    }
+});
+
+var routes = require('./routes/index');
+
+var accessLog = fs.createWriteStream('access.log', {flags: 'a'});
+var errorLog = fs.createWriteStream('error.log', {flags: 'a'});
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+app.use(morgan({stream: accessLog}));
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+routes(app);
+
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
---
+++ config/default.js
@@ -0,0 +1,9 @@
+var config = {
+	session:{
+		secrets: 'salt'
+	},
+	root: 'http://localhost:7777'
+    //root: 'http://api.secretlove.top'
+};
+
+module.exports = config;
